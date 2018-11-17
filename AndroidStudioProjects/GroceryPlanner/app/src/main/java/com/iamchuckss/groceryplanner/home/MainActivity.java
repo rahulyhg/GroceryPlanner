5f5e54b337e74f141bfe1ff24c5e5d2a09c72e11
Chuck <s2011297@gmail.com>
2018-11-17 10:15:13 +0800

    Initial Commit

---
+++ AndroidStudioProjects/GroceryPlanner/app/src/main/java/com/iamchuckss/groceryplanner/home/MainActivity.java
@@ -0,0 +1,42 @@
+package com.iamchuckss.groceryplanner.home;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.iamchuckss.groceryplanner.R;
+import com.iamchuckss.groceryplanner.utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int ACTIVITY_NUM = 0;
+
+    private Context mContext = MainActivity.this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView Setup
+     *
+     */
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
