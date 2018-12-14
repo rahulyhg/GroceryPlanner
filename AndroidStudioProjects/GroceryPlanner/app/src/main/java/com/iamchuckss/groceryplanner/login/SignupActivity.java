55dae10d9895bb91f0536b6e2015bd3b456568b6
Chuck <s2011297@gmail.com>
2018-12-14 13:39:21 +0800

    Added sign up feature

--- AndroidStudioProjects/GroceryPlanner/app/src/main/java/com/iamchuckss/groceryplanner/login/SignupActivity.java
+++ AndroidStudioProjects/GroceryPlanner/app/src/main/java/com/iamchuckss/groceryplanner/login/SignupActivity.java
@@ -2,20 +2,176 @@ package com.iamchuckss.groceryplanner.login;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.iamchuckss.groceryplanner.R;
+import com.iamchuckss.groceryplanner.models.User;
+import com.iamchuckss.groceryplanner.utils.FirebaseMethods;
 
-public class SignupActivity extends AppCompatActivity{
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpActivity";
 
-    private static final String TAG = "SignupActivity";
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseMethods firebaseMethods;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
 
-    Context mContext = SignupActivity.this;
+    private Context mContext;
+    private ProgressBar mProgressBar;
+    private String email, username, password;
+    private EditText mEmail, mPassword, mUserName;
+    private TextView mPleaseWait;
+    private Button btnRegister;
+
+    private String append = "";
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mContext = SignupActivity.this;
+        firebaseMethods = new FirebaseMethods(mContext);
+        Log.d(TAG, "onCreate: started");
+
+        initWidgets();
+        setupFirebaseAuth();
+        init();
+    }
+
+    private void init() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = mEmail.getText().toString();
+                username = mUserName.getText().toString();
+                password = mPassword.getText().toString();
+
+                if(checkInputs(email, username, password)) {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mPleaseWait.setVisibility(View.VISIBLE);
+
+                    firebaseMethods.registerNewEmail(email, password, username);
+                }
+            }
+        });
+    }
+
+    private boolean checkInputs(String email, String username, String password) {
+        Log.d(TAG, "checkInputs: checking inputs for null values");
+        if(email.equals("") || username.equals("") || password.equals("")) {
+            Toast.makeText(mContext, "All fields must be filled out", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Intialize the activity widgets
+     */
+    private void initWidgets() {
+        Log.d(TAG, "initWidgets: initializing widgets.");
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+        mUserName = (EditText) findViewById(R.id.inputUsername);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        mPleaseWait = (TextView) findViewById(R.id.pleaseWait);
+        mPleaseWait.setVisibility(View.GONE);
+        mEmail = (EditText) findViewById(R.id.inputEmail);
+        mPassword = (EditText) findViewById(R.id.inputPassword);
+    }
+
+    private boolean isStringNull(String string) {
+        Log.d(TAG, "isStringNull: checking string if null");
+
+        if(string.equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+     /*
+    -----------------------------------Firebase-----------------------------------------------
+     */
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // add new user to the database
+                            firebaseMethods.addNewUser(email, username);
+
+                            Toast.makeText(mContext, "Signup successful. Sending verification email.", Toast.LENGTH_SHORT).show();
+
+                            mAuth.signOut();
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    finish();
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out: ");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
---
+++ AndroidStudioProjects/GroceryPlanner/app/src/main/java/com/iamchuckss/groceryplanner/utils/FirebaseMethods.java
@@ -0,0 +1,88 @@
+package com.iamchuckss.groceryplanner.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.iamchuckss.groceryplanner.R;
+import com.iamchuckss.groceryplanner.models.User;
+
+public class FirebaseMethods {
+    private static final String TAG = "FirebaseMethods";
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String userID;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private StorageReference mStorageReference;
+
+    private Context mContext;
+
+    public FirebaseMethods(Context context) {
+        mContext = context;
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+
+        if(mAuth.getCurrentUser() != null) {
+            userID = mAuth.getCurrentUser().getUid();
+        }
+    }
+
+    /**
+     * Register a new email and password to Firebase authentication
+     * @param email
+     * @param password
+     * @param username
+     */
+    public void registerNewEmail(final String email, String password, final String username) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            userID = user.getUid();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(mContext, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    /**
+     * Add infromation to the users nodes
+     * Add information to the user_account_settings node
+     * @param email
+     * @param username
+     */
+    public void addNewUser(String email, String username) {
+        User user = new User(userID, email, username);
+
+        myRef.child(mContext.getString(R.string.dbname_users))
+                .child(userID)
+                .setValue(user);
+    }
+}
